ee7d5ff8858c8a7247f798b6661a3fb547c3638d
Joanne-Surface <afolabj@clarkson.edu>
2022-09-27 11:45:28 -0400

    Class structures

--- ClackData.java
+++ ClackData.java
@@ -1,12 +1,48 @@
 public class ClackData {
+    private String username; // name of client user
+    private int type; //
+    private int date; // date when ClackData object was
 
-    int username;
-    int type;
-    int date;
+    public void ClackData(userName,type){}
+    public void ClackData(type){}
+    public void ClackData(){}
+    public void getType(){
+        return type;
+    }
+    public void getUserName(){}
+    public void getDate(){}
 
-    ClackData(userName, type );
-    ClackData( type );
-    ClackData();
-    getType();
+    //Abstract Methods
 
+    getData(){}
+}
+
+public class MessageClackData extends ClackData{
+    private String message;
+
+    public void MessageClackData(userName,message,type){}
+    public void MessageClackData(){}
+    public void getData(){}
+    public void hashCode(){}
+    public void equals(){}
+    public void toString(){}
+
+}
+
+public class File Clack Data extends ClackData{
+    private String fileName;
+    private String fileContents;
+
+    public void FileClackData(userName,fileName,type){}
+    public void FileClackData(){}
+    public void setFileName(fileName){}
+    public void getFileName(){
+        return fileName;
+    }
+    public void getData(){}
+    public void readFileContents(){}
+    public void writeFileContents(){}
+    public void hashCode(){}
+    public void equals(){}
+    public void toString(){}
 }
